8f6eb963b51ac06dac9187973617dc88ea4d2ce1
tang <448915931@qq.com>
2020-06-26 18:28:28 +0800

    提交docker文件

--- tqfframe-website/src/main/java/com/tqfframe/website/controller/TestController.java
+++ tqfframe-website/src/main/java/com/tqfframe/website/controller/TestController.java
@@ -10,8 +10,14 @@ import org.springframework.web.bind.annotation.*;
 public class TestController {
 
     /**
+     *  本地
      *  http://localhost:8080/api/webiste/test/testhystrix/11111
      *  http://localhost:9091/test/testhystrix/11111
+     *
+     *  docker
+     *  http://192.168.47.135:8080/api/webiste/test/testhystrix/11111
+     *  http://192.168.47.135:9001/test/testhystrix/11111
+     *
      * @param name
      * @return
      */
