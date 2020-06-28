f70dba0f3ce471e28bdce7ece51dc7583e6470d5
tang <448915931@qq.com>
2020-06-28 11:39:37 +0800

    提交config配置中心

--- tqfframe-config/src/main/java/com/tqfframe/config/ConfigServerApplication.java
+++ tqfframe-config/src/main/java/com/tqfframe/config/ConfigServerApplication.java
@@ -2,15 +2,13 @@ package com.tqfframe.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 @EnableConfigServer
-@EnableDiscoveryClient
 public class ConfigServerApplication {
-
-    // TODO encrypt配置文件
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
