26b1d2fc1f6037dd932d0e62623ac870e4302f3c
tang <448915931@qq.com>
2020-06-30 11:07:06 +0800

    添加熔断监控

--- tqfframe-website/src/main/java/com/tqfframe/website/WebApplicationApp.java
+++ tqfframe-website/src/main/java/com/tqfframe/website/WebApplicationApp.java
@@ -2,7 +2,9 @@ package com.tqfframe.website;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -12,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
+@EnableCircuitBreaker //Hystrix仪表盘 服务熔断（ribbon 单服务实例监控）
 public class WebApplicationApp
 {
     public static void main( String[] args )
