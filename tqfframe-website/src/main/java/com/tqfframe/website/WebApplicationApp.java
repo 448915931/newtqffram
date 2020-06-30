48e1b96abbd44cba535aef699ef8e169ed8a3bb5
tang <448915931@qq.com>
2020-06-30 13:19:51 +0800

    添加熔断监控

--- tqfframe-website/src/main/java/com/tqfframe/website/WebApplicationApp.java
+++ tqfframe-website/src/main/java/com/tqfframe/website/WebApplicationApp.java
@@ -1,11 +1,14 @@
 package com.tqfframe.website;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Hello world!
@@ -21,4 +24,15 @@ public class WebApplicationApp
     {
         SpringApplication.run(WebApplicationApp.class, args);
     }
+
+
+//    @Bean
+//    public ServletRegistrationBean getServlet() {
+//        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+//        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+//        registrationBean.setLoadOnStartup(1);
+//        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+//        registrationBean.setName("HystrixMetricsStreamServlet");
+//        return registrationBean;
+//    }
 }
