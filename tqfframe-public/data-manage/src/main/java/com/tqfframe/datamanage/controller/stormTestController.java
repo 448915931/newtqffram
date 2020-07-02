8df938583420966ad3db7691d3b3a1d8371afa29
TangQiFeng <448915931@qq.com>
2020-07-02 13:39:52 +0800

    分布式数据处理中心

---
+++ tqfframe-public/data-manage/src/main/java/com/tqfframe/datamanage/controller/stormTestController.java
@@ -0,0 +1,37 @@
+package com.tqfframe.datamanage.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: 唐祺峰
+ * @Date: 2020/7/2 0002 13:37
+ * @Version 1.0
+ */
+@RestController
+public class stormTestController {
+
+    Logger logger = LoggerFactory.getLogger(stormTestController.class);
+
+    /**
+     *  本地
+     *  http://localhost:9009/test/11111
+     *
+     *  docker
+     *  http://192.168.47.135:9009/test/11111
+     *
+     * @param name
+     * @return
+     */
+    @GetMapping(value = "/test/{name}")
+    public String test(@PathVariable(name = "name") String name){
+        logger.info(name);
+        logger.debug(name);
+        logger.error(name);
+        return  "111";
+    }
+}
